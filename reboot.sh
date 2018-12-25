d21060b7c2c6efd542d8ffa16352b19728da73da
Tzu-Heng, Huang <35376020+zihengh1@users.noreply.github.com>
2018-12-25 09:41:31 +0800

    Update reboot.sh

--- reboot.sh
+++ reboot.sh
@@ -2,11 +2,11 @@
 
 sleep 1
 [ -f /home/pi/SensorArray_Without_Sigfox/sensor.py ] && {
-    /usr/bin/git -C /home/pi/SensorArray_Without_Sigfox fetch origin
-    /usr/bin/git -C /home/pi/SensorArray_Without_Sigfox reset --hard origin/master
-    sudo nohup python -u /home/pi/SensorArray_Without_Sigfox/sensor.py &
+    /usr/bin/sudo git -C /home/pi/SensorArray_Without_Sigfox fetch origin
+    /usr/bin/sudo git -C /home/pi/SensorArray_Without_Sigfox reset --hard origin/master
+    /usr/bin/sudo nohup python -u /home/pi/SensorArray_Without_Sigfox/sensor.py &
 } || {
-    /usr/bin/git clone https://github.com/zihengh1/SensorArray_Without_Sigfox/ /home/pi/SensorArray_Without_Sigfox
-    sudo nohup python -u /home/pi/SensorArray_Without_Sigfox/sensor.py &
+    /usr/bin/sudo git clone https://github.com/zihengh1/SensorArray_Without_Sigfox/ /home/pi/SensorArray_Without_Sigfox
+    /usr/bin/sudo nohup python -u /home/pi/SensorArray_Without_Sigfox/sensor.py &
 }
 
