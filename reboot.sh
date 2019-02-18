828963a43340f3ce5075733050ed818cd247af7f
ryan4559 <ryan4559@gmail.com>
2019-02-18 11:16:42 +0000

    fix interval in crontab

--- reboot.sh
+++ reboot.sh
@@ -2,14 +2,14 @@
 
 sleep 5
 #kill python run more then 1 day
-sudo killall -o 23h59m python
+#sudo killall -o 23h59m python
 
 [ -f /home/pi/SensorArray_Without_Sigfox/sensor.py ] && {
     /usr/bin/sudo git -C /home/pi/SensorArray_Without_Sigfox fetch origin
     /usr/bin/sudo git -C /home/pi/SensorArray_Without_Sigfox reset --hard origin/master
-    /usr/bin/sudo nohup python -u /home/pi/SensorArray_Without_Sigfox/sensor.py &
+    #/usr/bin/sudo nohup python -u /home/pi/SensorArray_Without_Sigfox/sensor.py &
 } || {
     /usr/bin/sudo git clone https://github.com/zihengh1/SensorArray_Without_Sigfox/ /home/pi/SensorArray_Without_Sigfox
-    /usr/bin/sudo nohup python -u /home/pi/SensorArray_Without_Sigfox/sensor.py &
+    #/usr/bin/sudo nohup python -u /home/pi/SensorArray_Without_Sigfox/sensor.py &
 }
 
--- sensor.py
+++ sensor.py
@@ -1,3 +1,7 @@
+#WARNING: This is a loop interval fixed version.
+#         Make sure your /etc/crontab is setting correct.
+#         */5 * * * * root exec /usr/bin/sudo python -u /home/pi/SensorArray_Without_Sigfox/sensor.py
+
 import serial
 import time
 import os
@@ -21,51 +25,51 @@ sigfox_id = -1
 path = "/home/pi/Data/"
 Restful_URL = "https://data.lass-net.org/Upload/SigFox.php"
 
-while True:
-    data = ""
-    now_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S").split(" ")
-    for i in range(0, 5):
-        if i is not sigfox_id:
-            try:
-                if(os.path.exists('/dev/ttyUSB' + str(i))):
-                    print("dev/ttyUSB " + str(i) + "exists")
-                    ser = serial.Serial(
-                        port = '/dev/ttyUSB'+str(i),
-                        baudrate = 9600,
-                        parity = serial.PARITY_NONE,
-                        stopbits = serial.STOPBITS_ONE, 
-                        bytesize = serial.EIGHTBITS,
-                  	)
-                    data += '|s%d:%d' % (i, int(ser.read(32)[7].encode('hex'), 16))
-                    ser.flushInput()
-                    ser.close()
-                    time.sleep(0.2)
-                else:
-                    print("dev/ttyUSB " + str(i) + "not exists")
-
-            except Exception as e:
+#while True:
+data = ""
+now_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S").split(" ")
+for i in range(0, 5):
+    if i is not sigfox_id:
+        try:
+            if(os.path.exists('/dev/ttyUSB' + str(i))):
+                print("dev/ttyUSB " + str(i) + "exists")
+                ser = serial.Serial(
+                    port = '/dev/ttyUSB'+str(i),
+                    baudrate = 9600,
+                    parity = serial.PARITY_NONE,
+                    stopbits = serial.STOPBITS_ONE, 
+                    bytesize = serial.EIGHTBITS,
+              	)
+                data += '|s%d:%d' % (i, int(ser.read(32)[7].encode('hex'), 16))
+                ser.flushInput()
                 ser.close()
-                print "serial.port is closed"
-                print(e)
-    if len(data):
-        data += '|%s_%s' % (str(now_time[0]), str(now_time[1]))
-        data_dict = Enc.split_string(data)
-        T3_binstr = Enc.dec_to_binstr(data_dict)
-        T3_hexstr = Enc.bin_to_hex(T3_binstr)
-        print "T3_hexstr : ", T3_hexstr
-    
-        try: 
-            restful_str3 = "wget -O /tmp/last_upload.log \"" + Restful_URL + "?device_id=" + device_id + "&data=" + T3_hexstr + "\""
-            os.system(restful_str3)
-        except Exception as e:
-            print(e)
+                time.sleep(0.2)
+            else:
+                print("dev/ttyUSB " + str(i) + "not exists")
 
-        data += '|' + device_id
- 	
-        with open(path + str(now_time[0]) + ".txt", "a") as f:
-            try: 
-                f.write(data + "\n")
-            except:
-                print "Error: writing to SD"	
-    
-    time.sleep(282)
+        except Exception as e:
+            ser.close()
+            print "serial.port is closed"
+            print(e)
+if len(data):
+    data += '|%s_%s' % (str(now_time[0]), str(now_time[1]))
+    data_dict = Enc.split_string(data)
+    T3_binstr = Enc.dec_to_binstr(data_dict)
+    T3_hexstr = Enc.bin_to_hex(T3_binstr)
+    print "T3_hexstr : ", T3_hexstr
+
+    try: 
+        restful_str3 = "wget -O /tmp/last_upload.log \"" + Restful_URL + "?device_id=" + device_id + "&data=" + T3_hexstr + "\""
+        os.system(restful_str3)
+    except Exception as e:
+        print(e)
+
+    data += '|' + device_id
+
+    with open(path + str(now_time[0]) + ".txt", "a") as f:
+        try: 
+            f.write(data + "\n")
+        except:
+            print "Error: writing to SD"	
+
+    #time.sleep(282)
