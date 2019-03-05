cd37f58ab51ad458f3066a723233a535a17f5f25
Ryan Weng <ryan4559@gmail.com>
2019-03-05 19:31:16 +0800

    Update reboot.sh

--- reboot.sh
+++ reboot.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 sleep 10
-#kill python run more then 1 day
-#sudo killall -o 23h59m python
 
 [ -f /home/pi/SensorArray_Without_Sigfox/sensor.py ] && {
     /usr/bin/sudo git -C /home/pi/SensorArray_Without_Sigfox remote update
