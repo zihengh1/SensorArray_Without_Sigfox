f0b1d316c976755e9d6b0fa5ad89947306aaff05
Ryan Weng <ryan4559@gmail.com>
2019-03-05 19:22:18 +0800

    Update reboot.sh

--- reboot.sh
+++ reboot.sh
@@ -5,6 +5,7 @@ sleep 5
 #sudo killall -o 23h59m python
 
 [ -f /home/pi/SensorArray_Without_Sigfox/sensor.py ] && {
+    /usr/bin/sudo git -C /home/pi/SensorArray_Without_Sigfox remote update
     /usr/bin/sudo git -C /home/pi/SensorArray_Without_Sigfox fetch origin
     /usr/bin/sudo git -C /home/pi/SensorArray_Without_Sigfox reset --hard origin/master
     #/usr/bin/sudo nohup python -u /home/pi/SensorArray_Without_Sigfox/sensor.py &
